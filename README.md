71eb60a643a431828f0bf40f752cd8ea6610f472
7XANAX7 <xanx57652@gmail.com>
2024-08-31 09:05:41 +0300

    X
    X

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# x-converter-script
- 
+<!-- # **Fivem Script Converter** -->
+
+<p align="center">
+  <img src="https://i.imgur.com/4iH7Wkm.gif" alt="animated" width="600" heigth="200"/>
+</p>
+
+---
+
+<p align="center">
+  <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/AmpedScripts/Fivem-Script-Converter">
+  <img src="https://img.shields.io/github/issues/AmpedScripts/Fivem-Script-Converter">
+  <img src="https://img.shields.io/github/issues-closed-raw/AmpedScripts/Fivem-Script-Converter">
+  <img src="https://img.shields.io/github/issues-pr-closed-raw/AmpedScripts/Fivem-Script-Converter">
+  </br>
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/AmpedScripts/Fivem-Script-Converter">
+  <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/AmpedScripts/Fivem-Script-Converter">
+  </br>
+</p>
+
+---
+
+# THIS IS A LIBRARY THIS WILL ONLY WORK IN JS USE THE CLI FOR NORMAL USE
+
+---
+## How it works 
+
+This libary can convert FiveM ESX scripts to Qbus and Qbus to ESX
+
+---
+
+### Usage
+
+```js
+const converter = require('fivem-script-converter');
+
+// to convert a qbus script to esx
+converter.qbustoesx('path/to/qbus/script.lua');
+// ===============================
+
+
+// to convert a esx script to qbus
+converter.esxtoqbus('path/to/esx/script.lua');
+// ===============================
+```
+
+---
+
+# Fivem Script Converter CLI
+<a href="https://github.com/AmpedScripts/Fivem-Script-Converter-CLI" target="blank"><img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"/>
+
+## Discord
+<a href="https://discord.com/users/670792646494650408" target="blank"><img src="https://img.shields.io/badge/Discord-100000?style=for-the-badge&logo=discord&logoColor=white"/>
+
+## Npm Page
+<a href="https://www.npmjs.com/package/fivem-script-converter" target="blank"><img src="https://img.shields.io/badge/Npm-100000?style=for-the-badge&logo=npm&logoColor=white"/>
---
+++ index.js
@@ -0,0 +1,216 @@
+const fs = require('fs');
+const colors = require('colors');
+var requestify = require('requestify');
+var pjson = require('./package.json');
+
+console.log("XXXXXXX       XXXXXXX               AAA               NNNNNNNN        NNNNNNNN               AAA               XXXXXXX       XXXXXXX");
+console.log("X:::::X       X:::::X              A:::A              N:::::::N       N::::::N              A:::A              X:::::X       X:::::X");
+console.log("X:::::X       X:::::X             A:::::A             N::::::::N      N::::::N             A:::::A             X:::::X       X:::::X");
+console.log("X::::::X     X::::::X            A:::::::A            N:::::::::N     N::::::N            A:::::::A            X::::::X     X::::::X");
+console.log("XXX:::::X   X:::::XXX           A:::::::::A           N::::::::::N    N::::::N           A:::::::::A           XXX:::::X   X:::::XXX");
+console.log("   X:::::X X:::::X             A:::::A:::::A          N:::::::::::N   N::::::N          A:::::A:::::A             X:::::X X:::::X   ");
+console.log("    X:::::X:::::X             A:::::A A:::::A         N:::::::N::::N  N::::::N         A:::::A A:::::A             X:::::X:::::X    ");
+console.log("     X:::::::::X             A:::::A   A:::::A        N::::::N N::::N N::::::N        A:::::A   A:::::A             X:::::::::X     ");
+console.log("     X:::::::::X            A:::::A     A:::::A       N::::::N  N::::N:::::::N       A:::::A     A:::::A            X:::::::::X     ");
+console.log("    X:::::X:::::X          A:::::AAAAAAAAA:::::A      N::::::N   N:::::::::::N      A:::::AAAAAAAAA:::::A          X:::::X:::::X    ");
+console.log("   X:::::X X:::::X        A:::::::::::::::::::::A     N::::::N    N::::::::::N     A:::::::::::::::::::::A        X:::::X X:::::X   ");
+console.log("XXX:::::X   X:::::XXX    A:::::AAAAAAAAAAAAA:::::A    N::::::N     N:::::::::N    A:::::AAAAAAAAAAAAA:::::A    XXX:::::X   X:::::XXX");
+console.log("X::::::X     X::::::X   A:::::A             A:::::A   N::::::N      N::::::::N   A:::::A             A:::::A   X::::::X     X::::::X");
+console.log("X:::::X       X:::::X  A:::::A               A:::::A  N::::::N       N:::::::N  A:::::A               A:::::A  X:::::X       X:::::X");
+console.log("X:::::X       X:::::X A:::::A                 A:::::A N::::::N        N::::::N A:::::A                 A:::::A X:::::X       X:::::X");
+console.log("XXXXXXX       XXXXXXXAAAAAAA                   AAAAAAANNNNNNNN         NNNNNNNAAAAAAA                   AAAAAAAXXXXXXX       XXXXXXX");
+console.log("\n       [+] Fivem Qbus to Esx / Esx to Qbus Converter [+]              ".brightCyan)
+
+requestify.get('https://ampedscripts.nl/version.txt')
+  .then(function(response) {
+      // Get the response body (JSON parsed or jQuery object for XMLs)
+      var responselmae = response.getBody();
+      var version = pjson.version;
+      if (response.getBody() === `${version}\n`) {
+     } else {
+          console.log(`\n\nThis version is outdated make sure to npm i x-script-converter\n\nCurrent Version: ${version}\nNewest Version: ${responselmae}`);
+          process.exit();
+     }
+});
+     
+
+var esxtoqbus = function esxtoqbus(file){
+     var code = fs.readFileSync(file,'utf8')
+
+     code = code.replaceAll(`TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)`, `TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)`);
+     code = code.replaceAll(`while ESX == nil do`, `while QBCore == nil do`);
+     code = code.replaceAll(`ESX.TriggerServerCallback`, `QBCore.Functions.TriggerCallback`);
+     code = code.replaceAll(`ESX.Game.GetVehicleProperties`, `QBCore.Functions.GetVehicleProperties`);
+     code = code.replaceAll(`ESX.Game.DeleteVehicle`, `QBCore.Functions.DeleteVehicle`);
+     code = code.replaceAll(`ESX.Game.SpawnVehicle`, `QBCore.Functions.SpawnVehicle`);
+     code = code.replaceAll(`ESX.Game.GetClosestVehicle`, `QBCore.Functions.GetClosestVehicle`);
+     code = code.replaceAll(`ESX =					nil`, `QBCore = nil `);
+     code = code.replaceAll(`ESX = nil`, `QBCore = nil `);
+     code = code.replaceAll(`RegisterNetEvent('esx:playerLoaded'`, `RegisterNetEvent('QBCore:Client:OnPlayerLoaded'`);
+     code = code.replaceAll(`AddEventHandler('esx:playerLoaded',`, `AddEventHandler('QBCore:Client:OnPlayerLoaded',`);
+     code = code.replaceAll(`RegisterNetEvent('esx:setJob'`, `RegisterNetEvent('QBCore:Client:OnJobUptade'`);
+     code = code.replaceAll(`AddEventHandler('esx:setJob',`, `AddEventHandler('QBCore:Client:OnJobUptade', `);
+     code = code.replaceAll(`RegisterNetEvent('esx:onPlayerDeath'`, `RegisterNetEvent('QBCore:Client:OnPlayerUnload'`);
+     code = code.replaceAll(`AddEventHandler('esx:onPlayerDeath',`, `AddEventHandler('QBCore:Client:OnPlayerUnload',`);
+     code = code.replaceAll(`ESX.Game.GetClosestPlayer()`, `QBCore.Functions.GetClosestPlayer()`);
+     code = code.replaceAll(`ESX.UI.Menu.Open`, `QBCore.UI.Menu.Open`);
+     code = code.replaceAll(`ESX.UI.Menu.CloseAll()`, `QBCore.UI.Menu.CloseAll()`);
+     code = code.replaceAll(`xPlayer.getInventoryItem`, `xPlayer.Functions.GetItemByName `);
+     code = code.replaceAll(`RegisterNetEvent('esx:setJob'`, `RegisterNetEvent('QBCore:Client:OnJobUpdate'`);
+     code = code.replaceAll(`AddEventHandler('esx:setJob', function(job)`, `AddEventHandler('QBCore:Client:OnJobUpdate', function(job)`);
+     code = code.replaceAll(`xPlayer.removeAccountMoney('bank', amount)`, `ply.Functions.AddMoney('bank', amount, "Bank depost")`);
+     code = code.replaceAll(`xPlayer.addMoney(amount)`, `ply.Functions.RemoveMoney('cash', amount, "Bank depost")`);
+     code = code.replaceAll(`xPlayer.getAccount('bank').money`, `ply.PlayerData.money["bank"]`);
+     code = code.replaceAll(`xPlayer.removeInventoryItem `, `xPlayer.Functions.RemoveItem `);
+     code = code.replaceAll(`xPlayer.addInventoryItem`, `xPlayer.Functions.AddItem`);
+     code = code.replaceAll(`ESX.GetPlayerFromId`, `QBCore.Functions.GetPlayer`);
+     code = code.replaceAll(`ESX.GetPlayerFromIdentifier`, `QBCore.Functions.GetPlayerByCitizenId`);
+     code = code.replaceAll(`ESX.Math.Trim(value)`, `QBCore.Functions.MathTrim`);
+     code = code.replaceAll(`ESX.GetPlayerData()`, `QBCore.Functions.GetPlayerData()`);
+     code = code.replaceAll(`ESX.RegisterUsableItem()`, `QBCore.Functions.CreateUseableItem()`);
+     code = code.replaceAll(`xPlayer.removeMoney(money)`, `Player.Functions.RemoveMoney()`);
+     code = code.replaceAll(`ESX.RegisterServerCallback()`, `QBCore.Functions.CreateCallback()`);
+     code = code.replaceAll(`ESX.TriggerServerCallback()`, `QBCore.Functions.TriggerCallback()`);
+     code = code.replaceAll(`ESX.Game.SetVehicleProperties`, `QBCore.Functions.SetVehicleProperties`);
+     code = code.replaceAll(`LoadModel`, `QBCore.Functions.LoadModel`);
+     code = code.replaceAll(`ESX.RegisterServerCallback("system:fetchStatus", function(source, cb)
+     local src = source
+     local user = ESX.GetPlayerFromId(src)
+     local fetch = [[
+          SELECT
+               skills
+          FROM
+               users
+          WHERE
+               identifier = @identifier
+     ]]
+     MySQL.Async.fetchScalar(fetch, {
+          ["@identifier"] = user.identifier
+     }, function(status)
+          if status ~= nil then
+               cb(json.decode(status))
+          else
+               cb(nil)
+          end
+     end)
+     end)`, `QBCore.Functions.CreateCallback('system:fetchStatus', function(source, cb)
+     local Player = QBCore.Functions.GetPlayer(source)
+      if Player then
+            exports['ghmattimysql']:execute('SELECT skills FROM players WHERE citizenid = @citizenid', {
+                ['@citizenid'] = Player.PlayerData.citizenid
+           }, function(status)
+               if status ~= nil then
+                    cb(json.decode(status))
+               else
+                    cb(nil)
+               end
+           end)
+      else
+           cb()
+      end
+     end)`);
+     code = code.replaceAll(`RegisterCommand `, `QBCore.Commands.Add()`);
+     code = code.replaceAll(`local user = ESX.GetPlayerFromId(src)`, `local Player = QBCore.Functions.GetPlayer(source)`);
+     code = code.replaceAll(`["@identifier"] = user.identifier`, `['@citizenid'] = Player.PlayerData.citizenid`);
+
+     fs.writeFileSync('output.lua', code, function (err) {
+         if (err) return console.log(err);
+     });
+
+     console.log(`Succesfully converted the ${file} from ESX to Qbus!`)
+};
+
+var qbustoesx = function qbustoesx(file){
+     var code = fs.readFileSync(file,'utf8')
+     code = code.replaceAll(`TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)`, `TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)`);
+     code = code.replaceAll(`QBCore = exports['qb-core']:GetCoreObject()`, `TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)`);
+     code = code.replaceAll(`local QBCore = exports['qb-core']:GetCoreObject()`,`ESX = nil\nTriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)`);
+     code = code.replaceAll(`while QBCore == nil do`, `while ESX == nil do`);
+     code = code.replaceAll(`QBCore.Functions.TriggerCallback`, `ESX.TriggerServerCallback`);
+     code = code.replaceAll(`QBCore.Functions.GetVehicleProperties`, `ESX.Game.GetVehicleProperties`);
+     code = code.replaceAll(`QBCore.Functions.DeleteVehicle`, `ESX.Game.DeleteVehicle`);
+     code = code.replaceAll(`QBCore.Functions.SpawnVehicle`, `ESX.Game.SpawnVehicle`);
+     code = code.replaceAll(`QBCore.Functions.GetClosestVehicle`, `ESX.Game.GetClosestVehicle`);
+     code = code.replaceAll(`QBCore = nil`, `ESX = nil`);
+     code = code.replaceAll(`QBCore == nil`, `ESX == nil`);
+     code = code.replaceAll(`RegisterNetEvent('QBCore:Client:OnPlayerLoaded'`, `RegisterNetEvent('esx:playerLoaded'`);
+     code = code.replaceAll(`AddEventHandler('QBCore:Client:OnPlayerLoaded',`, `AddEventHandler('esx:playerLoaded',`);
+     code = code.replaceAll(`RegisterNetEvent('QBCore:Client:OnJobUptade'`, `RegisterNetEvent('esx:setJob'`);
+     code = code.replaceAll(`AddEventHandler('QBCore:Client:OnJobUptade', `, `AddEventHandler('esx:setJob',`);
+     code = code.replaceAll(`RegisterNetEvent('QBCore:Client:OnPlayerUnload'`, `RegisterNetEvent('esx:onPlayerDeath'`);
+     code = code.replaceAll(`AddEventHandler('QBCore:Client:OnPlayerUnload',`, `AddEventHandler('esx:onPlayerDeath',`);
+     code = code.replaceAll(`QBCore.Functions.GetClosestPlayer()`, `ESX.Game.GetClosestPlayer()`);
+     code = code.replaceAll(`QBCore.UI.Menu.Open`, `ESX.UI.Menu.Open`);
+     code = code.replaceAll(`QBCore.UI.Menu.CloseAll()`, `ESX.UI.Menu.CloseAll()`);
+     code = code.replaceAll(`xPlayer.Functions.GetItemByName `, `xPlayer.getInventoryItem`);
+     code = code.replaceAll(`RegisterNetEvent('QBCore:Client:OnJobUpdate'`, `RegisterNetEvent('esx:setJob'`);
+     code = code.replaceAll(`AddEventHandler('QBCore:Client:OnJobUpdate', function(job`, `AddEventHandler('esx:setJob', function(job)`);
+     code = code.replaceAll(`ply.Functions.AddMoney`, `xPlayer.removeAccountMoney`);
+     code = code.replaceAll(`ply.Functions.RemoveMoney`, `xPlayer.addMoney`);
+     code = code.replaceAll(`ply.PlayerData.money["bank"]`, `xPlayer.getAccount('bank').money`);
+     code = code.replaceAll(`xPlayer.Functions.RemoveItem `, `xPlayer.removeInventoryItem`);
+     code = code.replaceAll(`xPlayer.Functions.AddItem`, `xPlayer.addInventoryItem`);
+     code = code.replaceAll(`QBCore.Functions.GetPlayer`, `ESX.GetPlayerFromId`);
+     code = code.replaceAll(`QBCore.Functions.GetPlayerByCitizenId`, `ESX.GetPlayerFromIdentifier`);
+     code = code.replaceAll(`QBCore.Functions.MathTrim`, `ESX.Math.Trim(value)`);
+     code = code.replaceAll(`QBCore.Functions.GetPlayerData`, `ESX.GetPlayerData`);
+     code = code.replaceAll(`QBCore.Functions.CreateUseableItem`, `ESX.RegisterUsableItem`);
+     code = code.replaceAll(`Player.Functions.RemoveMoney`, `xPlayer.removeMoney`);
+     code = code.replaceAll(`QBCore.Functions.CreateCallback`, `ESX.RegisterServerCallback`);
+     code = code.replaceAll(`QBCore.Functions.TriggerCallback`, `ESX.TriggerServerCallback`);
+     code = code.replaceAll(`QBCore.Functions.CreateCallback('system:fetchStatus', function(source, cb)
+     local Player = QBCore.Functions.GetPlayer(source)
+
+      if Player then
+            exports['ghmattimysql']:execute('SELECT skills FROM players WHERE citizenid = @citizenid', {
+                ['@citizenid'] = Player.PlayerData.citizenid
+           }, function(status)
+               if status ~= nil then
+                    cb(json.decode(status))
+               else
+                    cb(nil)
+               end
+           end)
+      else
+           cb()
+      end
+     end)`, `ESX.RegisterServerCallback("system:fetchStatus", function(source, cb)
+     local src = source
+     local user = ESX.GetPlayerFromId(src)
+
+
+     local fetch = [[
+          SELECT
+               skills
+          FROM
+               users
+          WHERE
+               identifier = @identifier
+     ]]
+
+     MySQL.Async.fetchScalar(fetch, {
+          ["@identifier"] = user.identifier
+
+     }, function(status)
+
+          if status ~= nil then
+               cb(json.decode(status))
+          else
+               cb(nil)
+          end
+
+     end)
+     end)`);
+     code = code.replaceAll(`QBCore.Commands.Add`, `RegisterCommand`);
+     code = code.replaceAll(`local Player = QBCore.Functions.GetPlayer(source)`, `local user = ESX.GetPlayerFromId(src)`);
+     code = code.replaceAll(`['@citizenid'] = Player.PlayerData.citizenid`, `["@identifier"] = user.identifier`);
+
+     fs.writeFileSync('output.lua', code, function (err) {
+         if (err) return console.log(err);
+     });
+
+     console.log(`Succesfully converted the ${file} from Qbus to ESX!`)
+};
+
+module.exports.qbustoesx = qbustoesx;
+module.exports.esxtoqbus = esxtoqbus;
